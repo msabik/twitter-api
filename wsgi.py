65cf594bb6c959cc1c00559c5decf26f37095acc
msabik <martinsabik@gmail.com>
2019-04-04 17:08:05 +0200

    some changes

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,32 @@
 from app import create_app
+from app.apis.tweets import *
+from flask import jsonify
 
 application = create_app()
 
 if __name__ == '__main__':
     application.run(debug=True)
+
+
+@application.route('/api/v1/tweets/<int:tweet_id>', methods=['GET'])
+def read_tweet(tweet_id):
+    msg = TweetResource()
+    return jsonify(msg.get(tweet_id))
+
+
+@application.route('/api/v1/tweets', methods=['POST'])
+def post_tweet():
+    msg = Tweet()
+    msg.post()
+    return msg, 201
+
+
+@application.route('/api/v1/tweets/<int:tweet_id>', methods=['DELETE'])
+def delete_tweet(tweet_id):
+    msg = TweetResource()
+    msg.delete(tweet_id)
+    return None
+
+
+
+
